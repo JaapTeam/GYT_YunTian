81b254593fe546489c029dbeaa1da8df5c3e3b8a
keepRunM <411384073@qq.com>
2017-07-16 18:02:05 +0800

    - 完成日志输出实体与相关接口

---
+++ src/Zer.Services/Logs/ILogsService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Zer.Services.Logs.Dto;
+
+namespace Zer.Services.Logs
+{
+    public interface ILogsService:IDomainService<LogsDto,int>
+    {
+        List<LogsDto> GetListByFilterMatch(FilterMatchInputDto filterMatch);
+
+        List<LogsDto> GetListByIds(int[] ids);
+    }
+}
