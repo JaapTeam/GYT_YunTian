41ef92fcdb7c543163ea9fb3c7fbc025a7b74dea
keepRunM <411384073@qq.com>
2017-07-31 22:55:14 +0800

    - 新增Log Entiyi

--- src/Zer.Entities/Log.cs
+++ src/Zer.Entities/Log.cs
@@ -1,8 +1,24 @@
-﻿using Zer.Framework.Entities;
+﻿using System;
+using Zer.Framework.Attributes;
+using Zer.Framework.Entities;
+using Zer.Framework.Export.Attributes;
 
 namespace Zer.Entities
 {
     public class Log : EntityBase
     {
+        public int UserId { get; set; }
+
+        public int ActionType { get; set; }
+
+        public string ActionModel { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public string MAC { get; set; }
+
+        public string IP { get; set; }
+
+        public string Content { get; set; }
     }
 }
