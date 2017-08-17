120db38f41862ea579cdd754a266b8c4352f72a6
keepRunM <411384073@qq.com>
2017-08-17 23:35:51 +0800

    - 完成登录页面美化 - lng添加状态字段并完成修改状态功能

--- src/com.gyt.ms/Controllers/LogInfoController.cs
+++ src/com.gyt.ms/Controllers/LogInfoController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Zer.AppServices;
 using Zer.Framework.Export;
-using Zer.GytDto;
 
 namespace com.gyt.ms.Controllers
 {
