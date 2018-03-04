cc69eae8b33c50bc3e6160467d3941ec323f0b71
jaapyang <15873115257@qq.com>
2018-03-04 14:51:18 +0800

    change field length

---
+++ src/Zer.GytDataService/Migrations/201803040623315_sync_data_model.cs
@@ -0,0 +1,16 @@
+namespace Zer.GytDataService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sync_data_model : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
